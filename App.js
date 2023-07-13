02bd6760e152784d41ee10102d5dbb6ba9364bba
John Ross <jchris.ross@outlook.com>
2023-07-13 13:23:18 -0400

    messing around

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -14,15 +14,43 @@ export default function App() {
 
 function UsefulBox(){
   return (
-    <Text>Does this work? Open up App.js to start working on your app!</Text>
+    <Pressable style={styles.usefulbox}>
+      <View style={{paddingBottom:20, backgroundColor: '#0d0'}}>
+        <Image
+          style={{width:150, height:77}}
+          source={require('./img_licence/alberta.jpg')}
+        />
+      </View>
+      <View style={{}}>
+        <Text style={styles.licenseText}>🇨🇦 Alberta 🇨🇦</Text>
+        <Text style={styles.licenseText}>🇺🇸 Colorado 🇺🇸</Text>
+      </View>
+      
+      
+    </Pressable>
   )
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  usefulbox: {
+    height: 450,
+    width: 250,
+    paddingBottom : 10,
     backgroundColor: '#2ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  licenseText: {
+    color: '#000',
+    fontSize: 20,
+    backgroundColor: '#f0f'
+  }
 });

dc3d896387ed736ad1a443711e20d0511fd53fcb
John Ross <jchris.ross@outlook.com>
2023-07-14 06:55:38 -0400

    Fixed the licence card text

---
+++ LicenceCard.js
@@ -0,0 +1,58 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+
+
+
+const LicenceCard = ({ }) => {
+  const province = {
+    name: 'Alberta',
+    image: require('./img_licence/alberta.jpg'),
+    flag: <Text style={{textAlign: 'center',fontSize: 20,}}>🇨🇦</Text>,
+    country: 'CA'
+  };
+  // 🇺🇸 🇨🇦
+  return (
+    <Pressable style={styles.usefulbox}>
+      <View style={{paddingBottom:15, backgroundColor: '#2ff'}}>
+        <Image
+          style={{width:150, height:77}}
+          source={province.image}
+        />
+      </View>
+      <View style={{maxWidth: 150, flexWrap: 'wrap', flexDirection: 'row',}}>
+        <View style={{flex: 1, flexDirection: 'row', 
+        justifyContent: 'flex-end',paddingRight:10, alignItems:'center'}}>
+          {province.flag}
+        </View>
+        <Text style={styles.licenseText}>{province.name}</Text>
+        <View style={{flex: 1, flexDirection: 'row', 
+        justifyContent: 'flex-start',paddingLeft:10, alignItems:'center'}}>
+          {province.flag}
+        </View>
+      </View>
+      
+      
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  usefulbox: {
+    width: 250,
+    paddingVertical: 20,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  licenseText: {
+    color: '#000',
+    fontSize: 20,
+    maxWidth: 90,
+    minWidth: 20,
+    textAlign: 'center',
+  }
+});
+
+export default LicenceCard;
+
+
+

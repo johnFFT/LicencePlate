b9eb250c27b072d8d0b5f598ebad46f8f27c9b72
John Ross <jchris.ross@outlook.com>
2023-09-01 13:41:02 -0400

    modal integration, Licence Card dev

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import LicenceCard from './LicenceCard';
 import DATA from './data';
-import { SafeAreaView, StyleSheet, View, ScrollView, FlatList } from 'react-native';
-
+import { SafeAreaView, StyleSheet, View, ScrollView, FlatList, Text } from 'react-native';
+import { useState } from 'react';
 
 
 
 export default function App() {
 
-  //const [selectedId, setSelectedId] = useState();
+  const [selectedId, setSelectedId] = useState('');
 
   const renderItem = ({item}) => {
     //const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
     //const color = item.id === selectedId ? 'white' : 'black';
+
+    //const [seen,setSeen] = useState(false);
+    
+
+    const test2OnPress = () => {
+      console.log('yupee!');
+      //setSeen((prevState) => !prevState);
+      setSelectedId(item.name);
+      item.onPress();
+    };
 
     return (
       <LicenceCard
         name={item.name}
         image={item.image}
         flag={item.flag}
-        onPress = {() => item.onPress()}
+        onPress = {() => test2OnPress()}
         //onPress={() => setSelectedId(item.id)}
         //backgroundColor={backgroundColor}
         //textColor={color}
@@ -36,15 +46,21 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-    <FlatList 
-      data={DATA}
-      renderItem={renderItem}
-      //keyExtractor={item => item.id}
-      //extraData={selectedId}
-    />
+    <LicenceCard name={'Berta'} image={require('./img_licence/alberta.jpg')} flag={<Text style={{textAlign: 'center',fontSize: 20,}}>🇨🇦</Text>}/>
     </SafeAreaView>
   );
 }
+
+/*
+<FlatList 
+data={DATA}
+renderItem={renderItem}
+//keyExtractor={item => item.id}
+extraData={selectedId}
+/>
+*/
+
+
 
 
 
--- LicenceCard.js
+++ LicenceCard.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
-
+import { Image, Pressable, StyleSheet, Text, View, Modal } from 'react-native';
+import moment from 'moment';
 
 
 const LicenceCard = ({ name, image, flag }) => {
@@ -15,29 +15,56 @@ const LicenceCard = ({ name, image, flag }) => {
   // 🇺🇸 🇨🇦
 
   [seen, setSeen] = useState(false);
+  [eTime, setETime] = useState('');
+  [modalSee, setModalSee] = useState(false);
+
+  const testOnPress = () => {
+    setSeen((prevState) => !prevState);
+  }
+
+  // onPress={() => {setSeen((prevState) => !prevState)}}>
+
+  const longPressFun = () => {
+    setModalSee((seeModal) => !seeModal);
+    setETime(moment().format('lll'));
+  }
 
   return (
-    <Pressable style={[styles.usefulbox, {backgroundColor: seen ? styles.usefulbox.selectedColor : styles.usefulbox.backgroundColor}]} onPress={() => {setSeen((prevState) => !prevState)}}>
-      <View style={{paddingBottom:15, backgroundColor: seen ? styles.usefulbox.selectedColor : styles.usefulbox.backgroundColor}}>
-        <Image
-          style={{width:150, height:77}}
-          source={image}
-        />
-      </View>
-      <View style={{maxWidth: 150, flexWrap: 'wrap', flexDirection: 'row',}}>
-        <View style={{flex: 1, flexDirection: 'row', 
-        justifyContent: 'flex-end',paddingRight:10, alignItems:'center'}}>
-          {flag}
+    <View >
+      <Modal visible={modalSee} transparent={true} animationType="fade" onBackdropPress={() => setModalSee(false)}>
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#000000a0',
+          }}>
+          <View style={{backgroundColor:'#446683',marginVertical: 50, marginHorizontal: 10, width : 300, height : 400}}>
+            <Text>{eTime}</Text>
+            <Pressable onPress={() => {setModalSee(false)}}><Text style={{margin:10}}>Close this</Text></Pressable>
+          </View>
         </View>
-        <Text style={styles.licenseText}>{name}</Text>
-        <View style={{flex: 1, flexDirection: 'row', 
-        justifyContent: 'flex-start',paddingLeft:10, alignItems:'center'}}>
-          {flag}
+      </Modal>
+      <Pressable style={[styles.usefulbox, {backgroundColor: seen ? styles.usefulbox.selectedColor : styles.usefulbox.backgroundColor}]} onPress={() => testOnPress()} onLongPress={() => longPressFun()}>
+        <View style={{paddingBottom:15, backgroundColor: seen ? styles.usefulbox.selectedColor : styles.usefulbox.backgroundColor}}>
+          <Image
+            style={{width:150, height:77}}
+            source={image}
+          />
         </View>
-      </View>
-      
-      
-    </Pressable>
+        <View style={{maxWidth: 150, flexWrap: 'wrap', flexDirection: 'row',}}>
+          <View style={{flex: 1, flexDirection: 'row', 
+          justifyContent: 'flex-end',paddingRight:10, alignItems:'center'}}>
+            {flag}
+          </View>
+          <Text style={styles.licenseText}>{name}</Text>
+          <View style={{flex: 1, flexDirection: 'row', 
+          justifyContent: 'flex-start',paddingLeft:10, alignItems:'center'}}>
+            {flag}
+          </View>
+        </View>
+      </Pressable>
+    </View>
   )
 }
 
